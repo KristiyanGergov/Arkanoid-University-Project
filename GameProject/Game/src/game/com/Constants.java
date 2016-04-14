0d4e4b0147fed5a86f37d27ec144b95062bed489
ngeorgieva <nat.m.georgieva@gmail.com>
2016-04-14 22:50:21 +0300

    Centered ball position

--- GameProject/Game/src/game/com/Constants.java
+++ GameProject/Game/src/game/com/Constants.java
@@ -8,7 +8,7 @@ public final class Constants {
     public static final int N_OF_BRICKS = 30;
     public static final int INIT_PADDLE_X = 110;
     public static final int INIT_PADDLE_Y = 360;
-    public static final int INIT_BALL_X = 160;
+    public static final int INIT_BALL_X = 140;
     public static final int INIT_BALL_Y = 347;
     public static final int DELAY = 1000;
     public static final int PERIOD = 10;
